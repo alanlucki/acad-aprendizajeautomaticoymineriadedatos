f8a2c9c7026f7e3822ef98c475e77a648a259b2d
Lucki Alan Fernandez Salcedo <alan.lucki@gmail.com>
2021-02-12 21:53:06 -0500

    varios

--- examen-parcial/01-descarga-audio.py
+++ examen-parcial/01-descarga-audio.py
@@ -1,5 +1,10 @@
 import os
 import librosa
+import ffmpy
+
+import wave
+import contextlib
+
 from shutil import rmtree
 from pytube import YouTube
 
@@ -25,13 +30,26 @@ def descarga(x,y):
     print('Finalizo la descarga de video de ', y)
   
 def convertir(x):
-    import ffmpy
-
+    
     origen = './mp4/audio/' + x + '.mp4'
     destino = './wav/' + x
+    os.system('ffmpeg  -ss 00:00:00 -t 00:00:10 -i {} -acodec pcm_s16le -ar 44000 {}.wav'.format(origen,destino ) ) 
 
-    os.system('ffmpeg  -ss 00:00:00 -t 00:00:20 -i {} -acodec pcm_s16le -ar 44000 {}.wav'.format(origen,destino ) ) 
+def segmentar(x,y):
+    
+    fname = './' + y + '.wav'
+    duration = 0
+    
+    
+    #for i in range(0, x):
+        #origen = './wav/' + x + '.wav'
+        #destino = './wav/' + x + '/' + x + "_" + i
+        #tiempo_inici = i * duracion_segmento
+        #tiempo_final = ( i + 1 ) * duracion_segmento
+        #os.system('ffmpeg  -ss 00:00:' + tiempo_inici +' -t 00:00:' + tiempo_final +' -i {} -acodec pcm_s16le -ar 44000 {}.wav'.format(origen,destino ) ) 
 
+    print (fname)
+    
 def procesar():
 
 
@@ -65,23 +83,32 @@ def procesar():
         '04-ceci-valen',
         '05-gonz-nunez',
         '06-dani-pered',
-        '07-marc-aurel',
+        '07-marc-deneg',
         '08-phil-butte',        
         '09-luis-trisa',
         '10-eric-osore'
 
     ]
     
- 
-    for x in range(0, len(links)):
+    
+    #for x in range(0, len(links)):
+    for x in range(0, 1):
         descarga(links[x] ,nombres[x] )
         convertir(nombres[x])
-
+        
+        #print("Fecuencia = " + fs)
+        #print("Periodo = " + (1.0/fs))
+        segmentar(10,'wav/' + nombres[x])
+        
+        #data, fs = librosa.load('wav/' + nombres[x] +'.wav')
+        
     try:
         rmtree("mp4")
     except:
         print("no existe")
 
-    #data, fs = librosa.load('wav/audio001.wav')
+    
+    #print data 
+    #print fs 
 
 procesar()
