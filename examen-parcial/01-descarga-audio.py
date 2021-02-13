d0a0a93c2cdd19be4cb3f885a4feb4ef6d3b0063
Lucki Alan Fernandez Salcedo <alan.lucki@gmail.com>
2021-02-12 20:31:23 -0500

    varios

--- examen-parcial/01-descarga-audio.py
+++ examen-parcial/01-descarga-audio.py
@@ -1,40 +1,86 @@
-'''
+import os
+from shutil import rmtree
 from pytube import YouTube
-link = 'https://www.youtube.com/watch?v=U7JizpfSc1U'
-youtube = YouTube(link)
-stream = youtube.streams.filter(only_audio=True).all()
-s.download('descarga')
-'''
-from pytube import YouTube
+
+def carpetas():
+    try:
+        rmtree("wav")
+    except:
+        print("no existe")
+    
+    try:
+        rmtree("mp4")
+    except:
+        print("no existe")
+
+    try:
+        os.mkdir('wav')
+    except:
+        print("ya existe")
     
 
-def descarga(link):
-    yt = YouTube(link)
-    
-    #stream = yt.streams.filter(only_audio=True).all()
-    #stream.download()
-    
+def descarga(x,y):
+    print('Inicio la descarga de video de ', y)
+    YouTube(x).streams.filter(only_audio=True).first().download('./mp4//audio/',y)
+    print('Finalizo la descarga de video de ', y)
+  
+def convertir(x):
+    import ffmpy
+
+    origen = './mp4/audio/' + x + '.mp4'
+    destino = './wav/' + x
+
+    os.system('ffmpeg -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(origen,destino ) ) 
 
 def procesar():
-    
+
+
     print ('===============')
     print ('Descargar audio')
     print ('===============')
     print ('Inicio')
     print ('======')
 
+    carpetas()
+
     links = [
-    
-        'https://www.youtube.com/watch?v=KcMBekiA8Sc'
-    
+
+        'https://www.youtube.com/watch?v=I087lKr0Z34&t=10s' , 
+        'https://www.youtube.com/watch?v=hAqB1WxkZR0&t=10s' ,
+        'https://www.youtube.com/watch?v=oNWGGTuAfTo&t=10s' ,
+        'https://www.youtube.com/watch?v=iNVrelV5YK0&t=10s' ,
+        'https://www.youtube.com/watch?v=QftIT_GeM74&t=10s' ,
+        'https://www.youtube.com/watch?v=62fhUNdSOR4&t=10s' ,
+        'https://www.youtube.com/watch?v=_HRJ-5sxg7s&t=10s' ,
+        'https://www.youtube.com/watch?v=ZmlwAXHVuFU&t=10s',
+        'https://www.youtube.com/watch?v=dcEMFdGaPAk&t=10s',
+        'https://www.youtube.com/watch?v=RfG59eqe_Zk&t=10s'
     ]
 
-    for x in links:
-        descarga(x)
+    nombres = [
+        
+        '01-beto-ortiz' ,
+        '02-rosamaria-palacios',
+        '03-cesar-hildrebrant',
+        '04-cecilia-valenzuela',
+        '05-gonzalo-nunez',
+        '06-daniel-peredo',
+        '07-marco-aurelio-denegri',
+        '08-phillip-butter',        
+        '09-luis-trisano',
+        '10-erick-osores'
 
-    print ('===')
-    print ('Fin')
-    print ('===')
+    ]
+    
+ 
+    for x in range(0, len(links)):
+    #for x in range(0, 1):
+        descarga(links[x] ,nombres[x] )
+        convertir(nombres[x])
 
+    try:
+        rmtree("mp4")
+    except:
+        print("no existe")
 
 procesar()
